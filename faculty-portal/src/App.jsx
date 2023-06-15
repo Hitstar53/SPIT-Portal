a53db06f264e64d3a27f125f686361d18e15dbf7
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-06-15 11:23:14 +0530

    sidebar component

--- faculty-portal/src/App.jsx
+++ faculty-portal/src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import './App.css'
+import SideBar from './components/SideBar'
 
 const App = () => {
   return (
     <React.Fragment>
     <div>
+      <SideBar />
       hello!!!
     </div>
     </React.Fragment>
