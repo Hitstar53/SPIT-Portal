9bc8089719722fb28621e5b5c16ab5063369c35a
Shubham More <97827118+shubhamore@users.noreply.github.com>
2023-06-15 12:56:18 +0530

    wrote basic code for viewProfile

--- faculty-portal/src/pages/ViewProfile.jsx
+++ faculty-portal/src/pages/ViewProfile.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Box } from '@mui/material';
+import '../styles/ViewProfile.css'
 
 const ViewProfile = () => {
     return (
-        <div>
-            Profile Page
+        <div className='full-view'>
+            <div className='wrapper'>
+                <div className='img-wrapper'>
+                    <img src='https://www.w3schools.com/howto/img_avatar.png' alt='profile' />
+                </div>
+                <div className='info-wrapper'>
+                    <h1 className='bigger'>Name : Shubham Ramdas More</h1>
+                    <h1 className='big'>Designation : Assistant Professor</h1>
+                    <h1 className='big'>Department : Computer Engineering</h1>
+                    <h1 className='big'>ID : 2021300079</h1>
+                </div>
+            </div>
+            <hr/>
         </div>
     );
 };
