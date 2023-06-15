abb1b387b8b9704e332b6d48e2bdb63c56e85367
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-06-15 12:55:33 +0530

    Routing functionality added

---
+++ faculty-portal/src/data/SideBarData.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import SchoolIcon from '@mui/icons-material/School';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstructions';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+
+export const SideBarData = [
+    {
+        title: 'Home',
+        icon: <HomeIcon />,
+        path: '/home'  
+    },
+    {
+        title: 'Classes',
+        icon: <SchoolIcon />,
+        path: '/classes'  
+    },
+    {
+        title: 'Attendance',
+        icon: <AssignmentIndIcon />,
+        path: '/attendance'  
+    },
+    {
+        title: 'Leaves',
+        icon: <LibraryBooksIcon />,
+        path: '/leaves'  
+    },
+    {
+        title: 'Mini Project',
+        icon: <IntegrationInstructionsIcon />,
+        path: '/mini-proj'  
+    },
+    {
+        title: 'Appraisal',
+        icon: <InsertDriveFileIcon />,
+        path: '/appraisal'  
+    },
+]
