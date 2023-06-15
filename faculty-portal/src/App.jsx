410eda75c22b798b83c49630062da2daee354ec6
Shubham <shubhamore02@gmail.com>
2023-06-15 11:42:32 +0530

    added sidebar in app.jsx

--- faculty-portal/src/App.jsx
+++ faculty-portal/src/App.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'
 import './App.css'
 import SideBar from './components/SideBar'
+import { Box } from '@mui/system'
 
 const App = () => {
   return (
-    <React.Fragment>
-    <div>
-      <SideBar />
-      hello!!!
+    <div className="App">
+      <Box sx={{ display: 'flex' }}>
+        <SideBar />
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: '#F5F6FA', p: 3 }}
+        >
+        
+        </Box>
+      </Box>
     </div>
-    </React.Fragment>
   )
 }
 
