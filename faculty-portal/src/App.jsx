9394b7f697b14aeaf941b0dade7051584b4eddf2
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-06-15 11:08:31 +0530

    faculty portal

---
+++ faculty-portal/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <React.Fragment>
+    <div>
+      hello!!!
+    </div>
+    </React.Fragment>
+  )
+}
+
+export default App
