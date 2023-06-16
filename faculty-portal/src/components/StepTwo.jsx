166d38cab992e0b35cc1238d2d43b8efab6a78a2
Shubham More <97827118+shubhamore@users.noreply.github.com>
2023-06-16 15:19:20 +0530

    step two basic code

--- faculty-portal/src/components/StepTwo.jsx
+++ faculty-portal/src/components/StepTwo.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { useForm, useFieldArray } from 'react-hook-form';
+import '../styles/Appraisal2.css'
 
-export default function StepTwo() {
+function StepTwo() {
+  const { register, control, handleSubmit } = useForm();
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'papers',
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // Perform any necessary actions with the form data, such as storing it in a database
+  };
+
   return (
-    <div>StepTwo</div>
-  )
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {fields.map((field, index) => (
+        <div key={field.id}>
+          <h3>Paper #{index + 1}</h3>
+          <label>Title</label>
+          <input {...register(`papers[${index}].title`)} />
+
+          <label>Journal</label>
+          <input {...register(`papers[${index}].journal`)} />
+
+          <label>Author</label>
+          <input {...register(`papers[${index}].author`)} />
+
+          <label>Publisher</label>
+          <input {...register(`papers[${index}].publisher`)} />
+
+          <label>Paper Link</label>
+          <input {...register(`papers[${index}].paperLink`)} />
+
+          <button type="button" onClick={() => remove(index)}>
+            Remove Paper
+          </button>
+        </div>
+      ))}
+
+      <button type="button" onClick={() => append({})}>
+        Add Paper
+      </button>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
+
+export default StepTwo;
