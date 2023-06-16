71de50fa34752b2a28f7bcf326dbad4a886c4489
Shubham More <97827118+shubhamore@users.noreply.github.com>
2023-06-16 11:05:15 +0530

    changed margin of icons in viewprofile

--- faculty-portal/src/pages/ViewProfile.jsx
+++ faculty-portal/src/pages/ViewProfile.jsx
@@ -36,25 +36,25 @@ const ViewProfile = () => {
           <div className="section">
             <h1 className="bigger">Personal</h1>
             <div className="info-wrapper">
-              <h1 className="bigg"><CalendarMonthIcon sx={{marginRight:2.5,marginTop:.7}}/>Date of Birth : 01/01/2000</h1>
-              <h1 className="bigg"><CalendarTodayIcon sx={{marginRight:2.5,marginTop:.7}}/>Date of Joining : 01/02/2022</h1>
-              <h1 className="bigg"><WcIcon sx={{marginRight:2.5,marginTop:.7}}/>Gender : Male</h1>
-              <h1 className="bigg"><BloodtypeIcon sx={{marginRight:2.5,marginTop:.7}}/>Blood Group : B+</h1>
-              <h1 className="bigg"><MergeTypeIcon sx={{marginRight:2.5,marginTop:.7}}/>Type : Regular</h1>
+              <h1 className="bigg"><CalendarMonthIcon sx={{marginRight:2.5,marginTop:.2}}/>Date of Birth : 01/01/2000</h1>
+              <h1 className="bigg"><CalendarTodayIcon sx={{marginRight:2.5,marginTop:.2}}/>Date of Joining : 01/02/2022</h1>
+              <h1 className="bigg"><WcIcon sx={{marginRight:2.5,marginTop:.2}}/>Gender : Male</h1>
+              <h1 className="bigg"><BloodtypeIcon sx={{marginRight:2.5,marginTop:.2}}/>Blood Group : B+</h1>
+              <h1 className="bigg"><MergeTypeIcon sx={{marginRight:2.5,marginTop:.2}}/>Type : Regular</h1>
             </div>
           </div>
           <div className="section" id="section-2">
             <h1 className="bigger">Contact</h1>
             <div className="info-wrapper">
-              <h1 className="bigg"><EmailIcon sx={{marginRight:2.5,marginTop:.7}}/>Email : shubham.more@spit.ac.in</h1>
-              <h1 className="bigg"><PhoneIcon sx={{marginRight:2.5,marginTop:.7}}/>Phone No. : 9975508234</h1>
+              <h1 className="bigg"><EmailIcon sx={{marginRight:2.5,marginTop:.2}}/>Email : shubham.more@spit.ac.in</h1>
+              <h1 className="bigg"><PhoneIcon sx={{marginRight:2.5,marginTop:.2}}/>Phone No. : 9975508234</h1>
               <h1 className="bigg">
-              <BusinessIcon sx={{marginRight:2.5,marginTop:.7}}/>
+              <BusinessIcon sx={{marginRight:2.5,marginTop:.2}}/>
                 Address : A/502, venice building, Mohak City, 90-Feet Road,
                 Andheri west
               </h1>
-              <h1 className="bigg"><GitHubIcon sx={{marginRight:2.5,marginTop:.7}}/>GitHub : Shubhamore</h1>
-            <h1 className="bigg"><LinkedInIcon sx={{marginRight:2.5,marginTop:.7}}/>LinkedIn : Shubhamore</h1>
+              <h1 className="bigg"><GitHubIcon sx={{marginRight:2.5,marginTop:.2}}/>GitHub : Shubhamore</h1>
+            <h1 className="bigg"><LinkedInIcon sx={{marginRight:2.5,marginTop:.2}}/>LinkedIn : Shubhamore</h1>
             </div>
           </div>
         </div>
