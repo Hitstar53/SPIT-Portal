56cb0440345429a6016175b6e63fff0ebf6e997d
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-06-19 09:57:51 +0530

    dummy data added

--- faculty-portal/src/data/TeacherData.jsx
+++ faculty-portal/src/data/TeacherData.jsx
@@ -1,35 +1,143 @@
-import React from 'react';
-
-export const AppraisalData = {
+export const TeacherData = {
     name: "John Doe",
-    year: "2021",
+    dob: "01/02/1995",
+    doj: "01/02/2020",
+    gender: "Male",
+    address:"123, ABC Street, XYZ City, 123456",
+    email: "example@spit.ac.in",
+    phone: "1234567890",
     designation: "Assistant Professor",
+    type: "Regular",
     department: "Computer Science and Engineering",
-    // courses: ['DAA', 'OS', 'DBMS'],
-    // classes: ['CSE-1A', 'CSE-1B', 'CSE-2C'],
-    // sem: [4, 4, 3],
-    // lecTarget: [
-    //     {
-    //         course: 'DAA',
-    //         target: String,
-    //     }
-    // ]
-    subjects: [
+    qualification: "PhD",
+    specialization:"Machine Learning, Artificial Intelligence, Image Processing, Computer Vision",
+    inchargeOf: "CSE-1A",
+    courses: [
         {
-            course: 'DAA',
+            courseName: 'DAA',
+            courseType: 'Core',
+            lecType: 'Theory',
             class: 'CSE-1A',
             sem: 4,
-            lecTarget: Number,
-            lecTaken: Number,
-            percentTarget: Number,
-            averagePercent: Number,
-            percentFeedback: Number,
-            menteeFeedback: {
-                type: Number(1, 2, 3, 4, 5), 
-                default: null
+            lecTarget: 40,
+            lecTaken: 38,
+            percentTarget: (lecTaken/lecTarget)*100,
+            lecPlan: [
+            {
+                lecNo: 1,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
             },
-            remedialActivity: String,
-            
-        }
+            {
+                lecNo: 2,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+            {
+                lecNo: 3,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+            {
+                lecNo: 4,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+        ],
+        announcement: [
+            {
+                date: '01/01/2023',
+                message: 'Class will be held on 02/01/2023',
+            },
+            {
+                date: '01/02/2023',
+                message: 'Class will be held on 02/02/2023',
+            },
+        ]
+        },
+        {
+            courseName: 'CCN',
+            courseType: 'Core',
+            lecType: 'Lab',
+            class: 'CSE-1A',
+            sem: 4,
+            lecTarget: 12,
+            lecTaken: 11,
+            percentTarget: (lecTaken/lecTarget)*100,
+            lecPlan: [
+            {
+                lecNo: 1,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+            {
+                lecNo: 2,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+            {
+                lecNo: 3,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+            {
+                lecNo: 4,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+        ],
+        announcement: [
+            {
+                date: '01/01/2023',
+                message: 'Class will be held on 02/01/2023',
+            },
+            {
+                date: '01/02/2023',
+                message: 'Class will be held on 02/02/2023',
+            },
+        ]
+        },
+        {
+            courseName: 'Cloud Computing',
+            courseType: 'OE',
+            lecType: 'Theory',
+            class: 'CSE-1A',
+            sem: 6,
+            lecTarget: 35,
+            lecTaken: 32,
+            percentTarget: (lecTaken/lecTarget)*100,
+            lecPlan: [
+            {
+                lecNo: 1,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+            {
+                lecNo: 2,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+            {
+                lecNo: 3,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+            {
+                lecNo: 4,
+                topic: 'Introduction',
+                suggestedReading: '1.1F',
+            },
+        ],
+        announcement: [
+            {
+                date: '01/01/2023',
+                message: 'Class will be held on 02/01/2023',
+            },
+            {
+                date: '01/02/2023',
+                message: 'Class will be held on 02/02/2023',
+            },
+        ]
+        },
     ]
 }
