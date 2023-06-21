1d1f20f8f2ff7135bfc12c066bf414c9bb4e66d6
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-06-21 11:12:35 +0530

    updated login page

--- faculty-portal/src/pages/Login.jsx
+++ faculty-portal/src/pages/Login.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import { React, useState } from "react";
+import axios from "axios";
 import "../styles/Login.css";
 
 export default function Login({ isLoggedIn, setIsLoggedIn}) {
+  const [email, setEmail] = useState("");
+
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/api/faculty/login", 
+    { email }).then((res) => {
+      console.log(res);
+      setIsLoggedIn(true);
+    }).catch((err) => {
+      console.log(err);
+    })
+  }
+
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form onSubmit={handleLogin} className="Auth-form">
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
           <div className="form-group mt-3">
@@ -13,6 +28,8 @@ export default function Login({ isLoggedIn, setIsLoggedIn}) {
               type="email"
               className="form-control mt-1"
               placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
