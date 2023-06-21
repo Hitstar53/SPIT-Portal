b2a93592782672e1e52f4839d2a7ac9db71f2437
Shubham More <97827118+shubhamore@users.noreply.github.com>
2023-06-21 10:56:49 +0530

    added login page and routes

--- faculty-portal/src/App.jsx
+++ faculty-portal/src/App.jsx
@@ -1,42 +1,52 @@
-import React, { useState } from 'react'
-import './App.css'
-import SideBar from './components/SideBar'
-import { Box } from '@mui/system'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
-import Classes from './pages/Classes'
-import Attendance from './pages/Attendance'
-import Leaves from './pages/Leaves'
-import MiniProj from './pages/MiniProj';
-import Appraisal from './pages/Appraisal';
-import ViewProfile from './pages/ViewProfile';
+import React, { useState } from "react";
+import "./App.css";
+import SideBar from "./components/SideBar";
+import { Box } from "@mui/system";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Classes from "./pages/Classes";
+import Attendance from "./pages/Attendance";
+import Leaves from "./pages/Leaves";
+import MiniProj from "./pages/MiniProj";
+import Appraisal from "./pages/Appraisal";
+import ViewProfile from "./pages/ViewProfile";
+import Login from "./pages/Login";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div className="App">
-      <Box sx={{ display: 'flex' }}>
-        <SideBar />
-        <Box
-          component="main"
-          sx={{ flexGrow: 1, bgcolor: '#F5F6FA', marginTop: 8,overflowX: 'hidden' }}
-        >
-          <Router>
-            <Routes>
-              {/* <Route path="/login" element={} /> */}
-              <Route path="/" element={<Home />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/profile" element={<ViewProfile />} />
-              <Route path="/classes" element={<Classes />} />
-              <Route path="/attendance" element={<Attendance />} />
-              <Route path="/leaves" element={<Leaves />} />
-              <Route path="/mini-proj" element={<MiniProj />} />
-              <Route path="/appraisal" element={<Appraisal />} />
-            </Routes>
-          </Router>
+      {isLoggedIn ? (
+        <Box sx={{ display: "flex" }}>
+          <SideBar />
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              bgcolor: "#F5F6FA",
+              marginTop: 8,
+              overflowX: "hidden",
+            }}
+          >
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/profile" element={<ViewProfile />} />
+                <Route path="/classes" element={<Classes />} />
+                <Route path="/attendance" element={<Attendance />} />
+                <Route path="/leaves" element={<Leaves />} />
+                <Route path="/mini-proj" element={<MiniProj />} />
+                <Route path="/appraisal" element={<Appraisal />} />
+              </Routes>
+            </Router>
+          </Box>
         </Box>
-      </Box>
+      ) : (
+        <Login />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ faculty-portal/src/pages/Login.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styles/Login.css";
+
+export default function Login() {
+  return (
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
