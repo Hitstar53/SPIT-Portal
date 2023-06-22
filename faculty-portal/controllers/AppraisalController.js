1ea608d67b45fb27d9336e1387144ac091762222
mahesh-patil20 <104311917+mahesh-patil20@users.noreply.github.com>
2023-06-22 12:40:52 +0530

    Dimension 3 and 4 updated in Controller
    Co-authored-by: Dhanashree Otari <dmo-27@users.noreply.github.com>

--- faculty-portal/controllers/AppraisalController.js
+++ faculty-portal/controllers/AppraisalController.js
@@ -14,26 +14,28 @@ const setAppraisal = asyncHandler(async (req, res) => {
 
         if (Dimension1.info.courses.length > 2) {
             avgAP1Marks = 10;
-        }
-        else {
+        } else {
             avgAP1Marks = Dimension1.info.courses.length * 4;
         }
         // ----------------------------------------------------------------------------------------
 
         for (var i = 0; i < Dimension1.info.courses.length; i++) {
-            total = total + Dimension1.info.courses[i].AP2MarksObtained
+            total = total + Dimension1.info.courses[i].AP2MarksObtained;
         }
         avgAP2Marks = total / Dimension1.info.courses.length;
 
         for (var i = 0; i < Dimension1.info.courses.length; i++) {
-            var averagePercent = (Dimension1.info.courses[i].LectureConducted / Dimension1.info.courses[i].LecturesTarget) * 100;
+            var averagePercent =
+                (Dimension1.info.courses[i].LectureConducted /
+                    Dimension1.info.courses[i].LecturesTarget) *
+                100;
             Dimension1.info.courses[i].AP3PercentAchieved = averagePercent;
-
         }
         var totalTarget = 0;
-        var averagePercent = 0
+        var averagePercent = 0;
         for (var i = 0; i < Dimension1.info.courses.length; i++) {
-            totalTarget = totalTarget + Dimension1.info.courses[i].AP3PercentAchieved
+            totalTarget =
+                totalTarget + Dimension1.info.courses[i].AP3PercentAchieved;
         }
         averagePercent = totalTarget / Dimension1.info.courses.length;
 
@@ -41,25 +43,26 @@ const setAppraisal = asyncHandler(async (req, res) => {
             //To be discussed
         }
         if (averagePercent >= 80 && averagePercent < 90) {
-            Dimension1.info.AP3Marks = 3
+            Dimension1.info.AP3Marks = 3;
         }
         if (averagePercent >= 90 && averagePercent < 95) {
-            Dimension1.info.AP3Marks = 4
+            Dimension1.info.AP3Marks = 4;
         }
         if (averagePercent >= 95 && averagePercent <= 100) {
-            Dimension1.info.AP3Marks = 5
+            Dimension1.info.AP3Marks = 5;
         }
         // -----------------------------------------------------------------------------------
 
         var totalpercentAP4 = 0;
-        var averagePercentAP4 = 0
+        var averagePercentAP4 = 0;
         for (var i = 0; i < Dimension1.info.courses.length; i++) {
-            totalpercentAP4 = totalpercentAP4 + Dimension1.info.courses[i].AP4PercentFeedback
+            totalpercentAP4 =
+                totalpercentAP4 + Dimension1.info.courses[i].AP4PercentFeedback;
         }
         averagePercentAP4 = totalpercentAP4 / Dimension1.info.courses.length;
 
         if (averagePercentAP4 > 30) {
-            averagePercentAP4 = 30
+            averagePercentAP4 = 30;
         }
         // -----------------------------------------------------------------------------------
         //AP5
@@ -88,9 +91,11 @@ const setAppraisal = asyncHandler(async (req, res) => {
         var totalMenteeFeedback = 0;
         var averageMenteeFeedback = 0;
         for (var i = 0; i < Dimension1.AP6.menteeFeedback.length; i++) {
-            totalMenteeFeedback = totalMenteeFeedback + Dimension1.AP6.menteeFeedback[i];
+            totalMenteeFeedback =
+                totalMenteeFeedback + Dimension1.AP6.menteeFeedback[i];
         }
-        averageMenteeFeedback = totalMenteeFeedback / Dimension1.AP6.menteeFeedback.length;
+        averageMenteeFeedback =
+            totalMenteeFeedback / Dimension1.AP6.menteeFeedback.length;
 
         if (averageMenteeFeedback > 5) {
             averageMenteeFeedback = 5;
@@ -115,8 +120,8 @@ const setAppraisal = asyncHandler(async (req, res) => {
 
         // ---------------------------------------------------------------------------------
 
-        Dimension1.info.AP1Marks = avgAP1Marks
-        Dimension1.info.AP2Average = avgAP2Marks
+        Dimension1.info.AP1Marks = avgAP1Marks;
+        Dimension1.info.AP2Average = avgAP2Marks;
         Dimension1.info.AP3Average = averagePercent;
         Dimension1.info.AP4Marks = averagePercentAP4;
         Dimension1.info.AP5Average = averageAttedanceAP5;
@@ -127,8 +132,6 @@ const setAppraisal = asyncHandler(async (req, res) => {
 
         // ==========================================================================================================
         //Dimension2 starts
-
-
 
         // =============================================================================================================
         //Dimension3 starts
@@ -149,17 +152,88 @@ const setAppraisal = asyncHandler(async (req, res) => {
                 Dimension3Marks = Dimension3Marks + 10;
             }
         }
+        // ------------------------------------------------------
+        //OP
+        var OPMarks = 0;
+        OPMarks = 3 * Dimension3.OP1.organized.length;
+        if (OPMarks > 15) {
+            OPMarks = 15;
+        }
+        // ------------------------------------------------------
+        //invited 
+        var invitedmarks = 0;
+        invitedmarks = 3 * Dimension3.Invited.invitedAt.length;
+        if (invitedmarks > 15) {
+            invitedmarks = 15;
+        }
+
+        // ------------------------------------------------------
+
+        var countCollab = Dimension3.collaboration.institutionDetails.length;
+        if (countCollab == 0) {
+            Dimension3.collaboration.totalMarks = 0;
+        } else if (countCollab <= 2) {
+            Dimension3.collaboration.totalMarks = 5 * countCollab;
+        } else {
+            Dimension3.collaboration.totalMarks = 10;
+        }
+
+        // ------------------------------------------------------------
+
+        var countCoGuide = Dimension3.coGuide.data.length;
+        if (countCoGuide == 0) {
+            Dimension3.coGuide.totalMarks = 0;
+        } else if (countCoGuide <= 2) {
+            Dimension3.coGuide.totalMarks = 5 * countCoGuide;
+        } else {
+            Dimension3.coGuide.totalMarks = 10;
+        }
+
+        // -----------------------------------------------
+
+        var countArticle = Dimension3.Article.articleDetails.length;
+        if (countArticle == 0) {
+            Dimension3.Article.totalMarks = 0;
+        } else if (countArticle <= 2) {
+            Dimension3.Article.totalMarks = 5 * countArticle;
+        } else {
+            Dimension3.Article.totalMarks = 10;
+        }
+
+        // ----------------------------------------
+
+
+        var countCommittee = Dimension3.Partof.committee.length;
+        if (countCommittee == 0) {
+            Dimension3.Article.totalMarks = 0;
+        } else if (countCommittee <= 3) {
+            Dimension3.Partof.totalMarks = 3 * countCommittee;
+        } else {
+            Dimension3.Partof.totalMarks = 10;
+        }
+
+        //   ------------------------------------------
 
         Dimension3.totalIP1IP2DP1Marks = Dimension3Marks;
+        Dimension3.OP1.totalMarks = OPMarks;
+        Dimension3.Invited.totalMarks = invitedmarks;
 
         // --------------------------------------------------------------------
         //Dimension4 starts
-        Dimension4.feedbackMarks.E = Dimension4.feedbackMarks.A + Dimension4.feedbackMarks.B + Dimension4.feedbackMarks.C + Dimension4.feedbackMarks.D;
-        Dimension4.confidentialReport.perceptionMarks = Dimension4.confidentialReport.principalRemarks * Dimension4.feedbackMarks.E;
+        Dimension4.feedbackMarks.E =
+            Dimension4.feedbackMarks.A +
+            Dimension4.feedbackMarks.B +
+            Dimension4.feedbackMarks.C +
+            Dimension4.feedbackMarks.D;
+        Dimension4.confidentialReport.perceptionMarks =
+            Dimension4.confidentialReport.principalRemarks *
+            Dimension4.feedbackMarks.E;
 
-
-        finalGrandTotal.GrandTotal = finalGrandTotal.dimension1.finalMarks + finalGrandTotal.dimension2.finalMarks + finalGrandTotal.dimension3.finalMarks + finalGrandTotal.dimension4.finalMarks;
-
+        finalGrandTotal.GrandTotal =
+            finalGrandTotal.dimension1.finalMarks +
+            finalGrandTotal.dimension2.finalMarks +
+            finalGrandTotal.dimension3.finalMarks +
+            finalGrandTotal.dimension4.finalMarks;
 
         // =============================================================================================================
         const newAppraisal = new Appraisal({
@@ -167,12 +241,10 @@ const setAppraisal = asyncHandler(async (req, res) => {
             Dimension2,
             Dimension3,
             Dimension4,
-            finalGrandTotal
+            finalGrandTotal,
         });
         const savedAppraisal = await newAppraisal.save();
         res.status(200).json(savedAppraisal);
-
-
     } catch (error) {
         console.error('Error saving appraisal:', error);
         throw error;
