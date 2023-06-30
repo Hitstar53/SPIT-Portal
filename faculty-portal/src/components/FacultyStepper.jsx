21da0b32e749f9fb6424038e5d02a55b46e80c1f
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-06-30 12:10:21 +0530

    stepper updated

--- faculty-portal/src/components/FacultyStepper.jsx
+++ faculty-portal/src/components/FacultyStepper.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import StepOne from './StepOne';
 import StepTwo from './StepTwo';
@@ -11,6 +11,10 @@ import StepFoot from './StepFoot';
 const steps = [`Dimension 1: Academics`, `Dimension 2: Research and Development`, `Dimension 3: Administration and Outreach `, `Dimension 4: Perception/ 360 degree feedback`];
 
 export default function FacultyStepper() {
+    const [Dimension1, setDimension1] = useState({})
+    const [Dimension2, setDimension2] = useState({})
+    const [Dimension3, setDimension3] = useState({})
+    const [Dimension4, setDimension4] = useState({})
 
     const [activeStep, setActiveStep] = React.useState(0);
     const handleNext = () => {
@@ -35,10 +39,10 @@ export default function FacultyStepper() {
                 <StepHead activeStep={activeStep} steps={steps} />
 
                 {/* render different pages depending upon activeStep */}
-                {activeStep === 0 && <StepOne />}
-                {activeStep === 1 && <StepTwo />}
-                {activeStep === 2 && <StepThree handleBack={handleBack} />}
-                {activeStep === 3 && <StepFour handleBack={handleBack} />}
+                {activeStep === 0 && <StepOne setDimension1={setDimension1}/>}
+                {activeStep === 1 && <StepTwo setDimension2={setDimension2}/>}
+                {activeStep === 2 && <StepThree setDimension3={setDimension3} handleBack={handleBack} />}
+                {activeStep === 3 && <StepFour setDimension4={setDimension4} handleBack={handleBack} />}
                 {/* {activeStep === 4 && <StepFour handleBack={handleBack} submitted={true} />} */}
 
                 {/* show footer only if all steps are not completed */}
