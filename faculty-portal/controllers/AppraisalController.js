82242dd196fb5fd47a835413f369f1dc8c0ce1bc
jrjaro18 <rohanjaiswal1500@gmail.com>
2023-07-03 10:58:02 +0530

    new

--- faculty-portal/controllers/AppraisalController.js
+++ faculty-portal/controllers/AppraisalController.js
@@ -6,7 +6,7 @@ const { error } = require("console");
 const setAppraisal = asyncHandler(async (req, res) => {
     console.log("Inside setAppraisalDim1");
     try {
-        var {yearofAssesment,facultyName,department,designation, Dimension1, Dimension2, Dimension3, Dimension4, finalGrandTotal } = req.body;
+        var { yearofAssesment, facultyName, department, designation, Dimension1, Dimension2, Dimension3, Dimension4, finalGrandTotal } = req.body;
         console.log(Dimension1);
         //Dimension1 starts
         var total = 0;
