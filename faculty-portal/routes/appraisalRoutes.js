212fb2a3fe394f2b24fd7bcec97cfae0a8fe3c80
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-07-04 15:04:46 +0530

    updated appraisal

--- faculty-portal/routes/appraisalRoutes.js
+++ faculty-portal/routes/appraisalRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const {
   setAppraisal,
-<<<<<<< HEAD
   setDim1,
   setDim2,
   setDim3,
@@ -12,12 +11,5 @@ const {
 router.post("/", setAppraisal);
 router.post("/dim1", setDim1);
 router.post("/dim2", setDim2);
-=======
-  getAppraisal,
-} = require("../controllers/AppraisalController");
-
-router.post("/", setAppraisal);
-router.post("/get", getAppraisal);
->>>>>>> 88c0962f54d00e147ed679e7d1de41bce5b361e4
 
 module.exports = router;
