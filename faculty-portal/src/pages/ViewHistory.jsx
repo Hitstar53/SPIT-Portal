3c5e7dd7dda70362a51ab80fef9180426f9f3d85
mahesh-patil20 <104311917+mahesh-patil20@users.noreply.github.com>
2023-07-05 15:01:48 +0530

    Some basic tables started on viewhistory page

--- faculty-portal/src/pages/ViewHistory.jsx
+++ faculty-portal/src/pages/ViewHistory.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useContext, useState } from 'react'
 import axios from 'axios';
 import { UserContext } from '../context/UserContext';
+
 const ViewHistory = () => {
     const { user } = useContext(UserContext);
-    const [history, setHistory] = useState([]);
+    const [history, setHistory] = useState({});
 
     useEffect(() => {
         const id = window.location.pathname.split('view-history/:')[1];
@@ -22,10 +23,55 @@ const ViewHistory = () => {
     }, [])
     return (
         <div>
-            {
-                history.facultyName
-            }
-        </div>
+            {history.facultyName}
+
+            < table >
+                <thead>
+                    <tr>
+                        <th>Year of Assessment:</th>
+                        <th>{history.yearofAssesment}</th>
+                        <th>Department</th>
+                        <th>{history.department}</th>
+                    </tr>
+                    <tr>
+                        <th>Name of Faculty</th>
+                        <th>{history.facultyName}</th>
+                        <th>Designation</th>
+                        <th>{history.designation}</th>
+                    </tr>
+                </thead>
+            </table >
+
+            {/* ---------------------------------------------------------- */}
+            < table >
+                <thead>
+                    AP1: Courses conducted
+                    <tr>
+                        <th>Sr. No.</th>
+                        <th>Course Name</th>
+                        <th>Class</th>
+                        <th>Semester</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        history.Dimension1.info.courses.map((course, index) => {
+                            <center>
+                                return (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{course.name}</td>
+                                    <td>{course.class}</td>
+                                    <td>{course.sem}</td>
+                                </tr>
+                                )
+                            </center>
+                        })
+                    }
+                </tbody>
+            </table >
+        </div >
     )
 }
 
