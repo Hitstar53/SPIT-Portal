18c9b0678f729b868d642e0ab90c3397e7e600f9
Siddhesh Sonar <sonarsiddhesh105@gmail.com>
2023-07-12 13:42:46 +0530

    principal entering marks done

---
+++ faculty-portal/src/components/Report.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import Table from "react-bootstrap/Table";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import axios from 'axios';
+import "../styles/Principal.css"
+
+const Report = ({ facultyData, name }) => {
+    console.log(facultyData)
+    const [dim4, setDim4] = useState(facultyData.Dimension4);
+    const marks = [
+        { label: '1'},
+        { label: '0.95'},
+        { label: '0.90'},
+        { label: '0.85'},
+        { label: '0.80'},
+    ]
+
+    const sendMarks = async (dim4) => {
+        console.log(dim4)
+        await axios.post("http://localhost:5000/api/faculty/appraisal/hodreview", {
+            yearofAssesment: facultyData.yearofAssesment,
+            fullName: facultyData.facultyName,
+            Dimension4: dim4,
+        })
+        .then((res) => {
+            console.log(res.data.Dimension4);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+
+    const handleMarks = (e) => {
+        e.preventDefault();
+        console.log("You clicked submit.");
+        const newPrincipalRemarks = parseFloat(e.target[0].value);
+        const updatedDim4 = {
+            ...dim4,
+            confidentialReport: {
+                ...dim4.confidentialReport,
+                principalRemarks: newPrincipalRemarks
+            }
+        };
+        // await setDim4(updatedDim4);
+        sendMarks(updatedDim4);
+    }
+    
+    
+
+    return (
+        <div>
+            <div className='report'>
+            <h1>Confidential Report for {name}</h1>
+                <Table striped bordered style={{margin: "1rem"}}>
+                    <thead>
+                        <tr>
+                            <th className='table-header text-center align-middle' colSpan={4}>Confidential Report</th>
+                        </tr>
+                        <tr>
+                            <th className='table-header text-center align-middle'>HOD Remarks</th>
+                            <th className='table-header text-center align-middle' colSpan={3}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis veniam consectetur nostrum quidem? Blanditiis atque optio corrupti voluptates, deleniti dolorem.</th>
+                        </tr>
+                        <tr>
+                            <th className='table-header text-center align-middle' rowSpan={6}>Principal Remarks</th>
+                            <th className='table-header text-center align-middle'>Multiplier factor (F)</th>
+                            <th className='table-header text-center align-middle'>Details</th>
+                            <th className='table-header text-center align-middle' rowSpan={6}>Marks E * F</th>
+                        </tr>
+                        <tr>
+                            <td className='table-content table-data text-center align-middle'>1</td>
+                            <td className='table-content table-data text-center align-middle'>Strongly agree: Contributor/ motivate others</td>
+                        </tr>
+                        <tr>
+                            <td className='table-content table-data text-center align-middle'>0.95</td>
+                            <td className='table-content table-data text-center align-middle'>Agree: performer / self-motivated</td>
+                        </tr>
+                        <tr>
+                            <td className='table-content table-data text-center align-middle'>0.90</td>
+                            <td className='table-content table-data text-center align-middle'>Neutral: committed / complete the tasks</td>
+                        </tr>
+                        <tr>
+                            <td className='table-content table-data text-center align-middle'>0.85</td>
+                            <td className='table-content table-data text-center align-middle'>Disagree: low commitments/ needs follow ups </td>
+                        </tr>
+                        <tr>
+                            <td className='table-content table-data text-center align-middle'>0.80</td>
+                            <td className='table-content table-data text-center align-middle'>Strongly disagree: not committed / needs frequent follow up</td>
+                        </tr>
+                    </thead>
+                </Table>
+                <form className='marks-sec' onSubmit={handleMarks}>
+                    <Autocomplete
+                        disablePortal
+                        id="combo-box-demo"
+                        options={marks}
+                        sx={{ width: 300, display: "inline-block" }}
+                        renderInput={(params) => (
+                            <TextField {...params} label="Enter Marks" />
+                        )}
+                    />
+                    <button type="submit" className="marks-btn btn btn-primary">
+                        Confirm Marks
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Report;
--- faculty-portal/src/pages/Principal.jsx
+++ faculty-portal/src/pages/Principal.jsx
@@ -5,16 +5,38 @@ import "../styles/DeptAppraisal.css";
 import { useContext } from "react";
 import { UserContext } from '../context/UserContext';
 import axios from 'axios';
+import Report from "../components/Report";
+import "../styles/Principal.css"
 
 const Principal = () => {
     const { user } = useContext(UserContext);
+    const [name, setName] = useState("");
+    const [status,setStatus]=useState("Not searched");
+    let report = null;
     const [facultyName, setfacultyName] = useState([]);
+    const [facultyData , setfacultyData] = useState([]);
+
+    function getDate() {
+      const year = new Date().getFullYear();
+      const month = new Date().getMonth() + 1;
+      const day = new Date().getDate();
+      // console.log(month);
+      // console.log(year);
+      // console.log(day);
+      // if(month<6)
+      return `${year}-${year + 1}`;
+      // else
+      // return `${year+1}-${year+2}`
+    }
+    
+    var yr = getDate();
+    
 
   useEffect(() => {
     const getAllFaculty = async () => {
-        axios.post("http://localhost:5000/api/faculty/get/faculty/all")
+        axios.post("http://localhost:5000/api/faculty/get/faculty/all-principal", { year: yr })
         .then((res) => {
-            console.log(res.data)
+            // console.log(res.data)
             setfacultyName(res.data.sort());
         })
         .catch((err) => {
@@ -24,11 +46,37 @@ const Principal = () => {
     getAllFaculty();
   }, []);
 
+  const viewReport = async (faculty) => {
+    await axios.post("http://localhost:5000/api/faculty/get/faculty/check-principal-faculty", {
+        name: faculty,
+        year: yr,
+      })
+      .then((res) => {
+        console.log(res.data);
+        console.log(res.status)
+        if(res.status===200) {
+          setStatus("Faculty found")
+          setfacultyData(res.data);
+        }
+        else {
+          setStatus("Faculty not found")
+        }
+      })
+      .catch((err) => {
+        setStatus("Faculty not found")
+        console.log(err);
+      })
+
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
     console.log("You clicked submit.");
+    setName(e.target[0].value);
     console.log(e.target[0].value);
+    viewReport(e.target[0].value);
   }
+
   return (
     <div className="dept-appraisal">
       <form onSubmit={handleSubmit}>
@@ -48,7 +96,11 @@ const Principal = () => {
         </div>
       </form>
       <div>
-        
+      <div className="dept-appraisal-body">
+        {status==="Not searched"&&<h1>Click on Find Faculty to Enter their marks</h1>}
+        {status==="Faculty found"&&<h1><Report facultyData={facultyData} name={name}/></h1>}
+        {status==="Faculty not found"&&<h1>Faculty not found</h1>}
+      </div>
       </div>
     </div>
   );
