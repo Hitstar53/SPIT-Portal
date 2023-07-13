602d52f02c119abaaadeb5e3d15f1ec5ebaf0b02
Shubham More <97827118+shubhamore@users.noreply.github.com>
2023-07-13 12:08:36 +0530

    added security in hod to fill appraisal once only

--- faculty-portal/src/pages/Appraisal.jsx
+++ faculty-portal/src/pages/Appraisal.jsx
@@ -1,24 +1,44 @@
-import * as React from 'react';
-import FacultyStepper from '../components/FacultyStepper';
-import '../styles/Appraisal.css';
-import { useContext,useEffect } from 'react';
-import { UserContext } from '../context/UserContext';
+import * as React from "react";
+import FacultyStepper from "../components/FacultyStepper";
+import "../styles/Appraisal.css";
+import { useState,useContext, useEffect } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Appraisal = () => {
-  const { user } = useContext(UserContext);
+  const { user,yr } = useContext(UserContext);
+  const [submitted, setSubmitted] = useState(false);
   useEffect(() => {
-    
-  }
-  , [user])
+    fetch(
+      "http://localhost:5000/api/faculty/appraisal/isSubmitted-teacher",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: user.fullName,
+          yearofAssesment: yr,
+        }),
+      }
+    ).then((res) => {
+      if (res.status === 200) {
+        setSubmitted(true);
+      } else if (res.status === 204) {
+        setSubmitted(false);
+      }
+    });
+  }, []);
 
   return (
-    <div className='appraisal-page'>
-      <div className='app-title'> 
-      <h1 className='ap-title'>Self-Appraisal Form for Faculty</h1>
-      <FacultyStepper/>
-      </div>     
+    <div className="appraisal-page">
+      <>
+        {!submitted?<div className="app-title">
+          <h1 className="ap-title">Self-Appraisal Form for Faculty</h1>
+          <FacultyStepper />
+        </div>:<h1>Already Submitted</h1>}
+      </>
     </div>
-  )
+  );
 };
 
 export default Appraisal;
