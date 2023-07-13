53c2c283b5159ef5d4aa5c3ad9e722983e845a68
Shubham More <97827118+shubhamore@users.noreply.github.com>
2023-07-13 14:02:12 +0530

    added illustrations for teacher appraisal, loading

--- faculty-portal/src/pages/Appraisal.jsx
+++ faculty-portal/src/pages/Appraisal.jsx
@@ -1,42 +1,67 @@
 import * as React from "react";
 import FacultyStepper from "../components/FacultyStepper";
 import "../styles/Appraisal.css";
-import { useState,useContext, useEffect } from "react";
+import { useState, useContext, useEffect } from "react";
 import { UserContext } from "../context/UserContext";
+import ProjectDone from "../assets/project-is-done.png";
+import { set } from "date-fns";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const Appraisal = () => {
-  const { user,yr } = useContext(UserContext);
+  const { user, yr } = useContext(UserContext);
   const [submitted, setSubmitted] = useState(false);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetch(
-      "http://localhost:5000/api/faculty/appraisal/isSubmitted-teacher",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: user.fullName,
-          yearofAssesment: yr,
-        }),
-      }
-    ).then((res) => {
+    fetch("http://localhost:5000/api/faculty/appraisal/isSubmitted-teacher", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: user.fullName,
+        yearofAssesment: yr,
+      }),
+    }).then((res) => {
       if (res.status === 200) {
         setSubmitted(true);
       } else if (res.status === 204) {
         setSubmitted(false);
       }
+      setLoading(false);
     });
   }, []);
 
   return (
-    <div className="appraisal-page">
-      <>
-        {!submitted?<div className="app-title">
-          <h1 className="ap-title">Self-Appraisal Form for Faculty</h1>
-          <FacultyStepper />
-        </div>:<h1>Already Submitted</h1>}
-      </>
+    <div style={{ height: "100%" }}>
+      {loading ? (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          <CircularProgress color="success" />
+        </div>
+      ) : (
+        <>
+          {!submitted ? (
+            <div className="appraisal-page">
+              <div className="app-title">
+                <h1 className="ap-title">Self-Appraisal Form for Faculty</h1>
+                <FacultyStepper />
+              </div>
+            </div>
+          ) : (
+            <div className="appraisal-center">
+              <h1>You Have Already Submitted Appraisal</h1>
+              <img src={ProjectDone} alt="submission-done" />
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
