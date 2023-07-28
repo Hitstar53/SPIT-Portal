0c4467d00503174dbaeb63447a1a7c988979ab78
Hatim Sawai <hatimsawai76@gmail.com>
2023-07-29 01:00:32 +0530

    ui changes in pdf rendering
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- client/src/components/Faculty/SearchFilters/ProfileDownload.jsx
+++ client/src/components/Faculty/SearchFilters/ProfileDownload.jsx
@@ -10,7 +10,6 @@ import DownloadIcon from '@mui/icons-material/Download';
 import Poppins from "../../../assets/Poppins-Regular.ttf"
 import PoppinsBold from "../../../assets/Poppins-Bold.ttf"
 import PoppinsSemiBold from "../../../assets/Poppins-SemiBold.ttf"
-import Logo from "../../../assets/spitlogo.jpg";
 
 Font.register({ family: 'Poppins', fonts:[
   {src:Poppins},
@@ -41,10 +40,6 @@ const ProfilePdf = ({data}) => {
         height:50,
         gap: 20
       },
-  
-    
-    
-  
     });
     
     return (
@@ -53,7 +48,7 @@ const ProfilePdf = ({data}) => {
           <View style={styles.section}>
             <View style={styles.title}><Text>Student Profile Of {data.name}</Text></View>
             <Svg height="10" width="580">
-              <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={3} stroke="rgb(0,0,0)"/>
+              <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={1} stroke="rgb(0,0,0)"/>
             </Svg>
             <View wrap={false}>
               <Heading heading="Personal Information" />
@@ -66,11 +61,11 @@ const ProfilePdf = ({data}) => {
               <Row heading="Gender" data={data.gender} />
               <Row heading="Blood Group" data={data.blood} />
               <Row heading="Religion" data={data.religion} />
-              <Svg height="10" width="580">
-                <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={3} stroke="rgb(0,0,0)"/>
+              <Svg style={{marginTop:15}} height="10" width="580">
+                <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={1} stroke="rgb(0,0,0)"/>
               </Svg>
             </View>
-            <View wrap={false}>
+            <View style={{marginTop:15}} wrap={false}>
               <Heading heading="Parental Information" />
               <Row heading="Father Name" data={data.fname} />
               <Row heading="Father Mobile" data={data.fphone} />
@@ -80,14 +75,14 @@ const ProfilePdf = ({data}) => {
               <Row heading="Mother Mobile" data={data.mphone} />
               <Row heading="Mother Email" data={data.memail} />
               <Row heading="Mother Occupation" data={data.mprofession} />
-              <Svg height="10" width="580">
-                <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={3} stroke="rgb(0,0,0)"/>
+              <Svg style={{marginTop:15}} height="10" width="580">
+                <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={1} stroke="rgb(0,0,0)"/>
               </Svg>
             </View>
-            <View style={{marginTop:15}} wrap={false}>
-              <Svg height="10" width="580">
-                <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={3} stroke="rgb(0,0,0)"/>
-              </Svg>
+            <View style={{marginTop:35}} wrap={false}>
+              {/* <Svg height="10" width="580">
+                <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={1} stroke="rgb(0,0,0)"/>
+              </Svg> */}
               <Heading heading="Educational Information" />
               <SubHeading heading="Current Details" />
               <Row heading="Institute" data={data.educationalInfo[0].insName} />
@@ -108,11 +103,10 @@ const ProfilePdf = ({data}) => {
               <Row heading="Board" data={data.educationalInfo[2].degree} />
               <Row heading="Score" data={data.educationalInfo[2].score} />
               <Row heading="Passing Year" data={data.educationalInfo[2].passingYear} />
-              <Svg height="10" width="580">
-                <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={3} stroke="rgb(0,0,0)"/>
+              <Svg style={{marginTop:15}} height="10" width="580">
+                <Line x1="0" y1="0" x2="560" y2="0" strokeWidth={1} stroke="rgb(0,0,0)"/>
               </Svg>
             </View>
-            
           </View>
         </Page>
       </Document>
@@ -130,22 +124,25 @@ const Row = ({heading,data}) =>{
 const SubHeading = ({heading}) =>{
   return(
     <View style={{flexDirection:"row"}} wrap={false}>
-      <Text style={{fontSize: 15,
-        fontWeight: 600,
-        textAlign: "left",
-        }}>{heading}</Text>
+      <Text style={{
+          fontSize: 15,
+          fontWeight: 600,
+          textAlign: "left",
+          marginTop: 15,
+        }}>
+          {heading}
+      </Text>
     </View>  
   )
 }
 const Heading = ({heading}) =>{
-  return(
-    <View style={{flexDirection:"row"}} wrap={false}>
-      <Text style={{fontSize: 20,
-        fontWeight: 600,
-        textAlign: "left",
-        }}>{heading}</Text>
-    </View>  
-  )
+  return (
+    <View style={{ flexDirection: "row" }} wrap={false}>
+      <Text style={{ fontSize: 20, fontWeight: 600, textAlign: "left" }}>
+        {heading}
+      </Text>
+    </View>
+  );
 }
 
 const ProfileDownload = () => {
@@ -154,7 +151,6 @@ const ProfileDownload = () => {
     const container = styles.container + " flex flex-col gap-8 p-8";
     return (
       <div className={container}>
-        {/* Make a grid table of 2 columns using tailwind, first column has property name of data object and second column will have the value */}
         <div className="flex justify-between items-center text-4xl font-semibold">
           <p className="flex items-center gap-4">
             <FaArrowLeft
